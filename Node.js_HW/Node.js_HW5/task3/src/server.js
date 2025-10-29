7e8a1ba923848060de9809b413a3717fe03a7418
Max <stigerok555@gmail.com>
2025-10-29 08:12:51 +0100

    chandged some logic

--- Node.js_HW/Node.js_HW5/task3/src/server.js
+++ Node.js_HW/Node.js_HW5/task3/src/server.js
@@ -4,19 +4,19 @@ const startServer = () => {
   const app = express();
 
   app.put("/", (req, res) => {
-    res.status(200).type("text/plain").send("PUT-запрос обработан");
+    return res.send("PUT-запрос обработан");
   });
 
   app.delete("/", (req, res) => {
-    res.status(200).type("text/plain").send("Delete-запрос обработан");
+    return res.send("DELETE-запрос обработан");
   });
 
   app.all("/", (req, res) => {
-    res.status(405).type("text/plain").send("Метод не поддерживается");
+    return res.status(405).send("Метод не поддерживается");
   });
 
   const port = Number(process.env.PORT) || 3000;
-  app.listen(port, () => console.log(`Server running on ${port} port`));
+  app.listen(port, () => console.log(`Server running on port ${port}`));
 };
 
 export default startServer;
