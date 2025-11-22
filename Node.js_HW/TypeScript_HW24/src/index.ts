a3c805182dde60b8342ab570de9e1197510ace8a
Max <stigerok555@gmail.com>
2025-11-22 11:40:22 +0100

    changed index.js on TS with types

--- Node.js_HW/TypeScript_HW24/src/index.ts
+++ Node.js_HW/TypeScript_HW24/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
 
-app.post("/data", (req, res) => {
+app.post("/data", (req: Request, res: Response) => {
   res.json({ received: req.body });
 });
 
